d95373381a64714a45601465d9d19bcdfa37d45b
Benedict <benedictryan80@gmail.com>
2022-07-11 18:56:50 +0700

    add react router dom

--- src/App.js
+++ src/App.js
@@ -2,9 +2,18 @@ import { useEffect } from 'react';
 
 import './App.css';
 import ContactUs from './components/ContactUs';
+
 import ReactGA from 'react-ga';
+import { Routes, Route } from 'react-router-dom';
+import About from './components/About';
 const TRACKING_ID = 'UA-189719752-1';
 ReactGA.initialize(TRACKING_ID);
+
+const Home = () => (
+	<div>
+		<h1>Home</h1>
+	</div>
+);
 
 function App() {
 	useEffect(() => {
@@ -12,7 +21,12 @@ function App() {
 	}, []);
 	return (
 		<div className="App">
-			<ContactUs />
+			<h1>React Google Analytics</h1>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/contact" element={<ContactUs />} />
+				<Route path="/about" element={<About />} />
+			</Routes>
 		</div>
 	);
 }
