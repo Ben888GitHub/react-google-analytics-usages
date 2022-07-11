e882b118e79f884c79d657162d902031eaba715a
Benedict <benedictryan80@gmail.com>
2022-07-11 18:45:38 +0700

    add pageview from ReactGA

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useEffect } from 'react';
+
 import './App.css';
 import ContactUs from './components/ContactUs';
 import ReactGA from 'react-ga';
@@ -5,6 +7,9 @@ const TRACKING_ID = 'UA-189719752-1';
 ReactGA.initialize(TRACKING_ID);
 
 function App() {
+	useEffect(() => {
+		ReactGA.pageview(window.location.pathname + window.location.search);
+	}, []);
 	return (
 		<div className="App">
 			<ContactUs />
